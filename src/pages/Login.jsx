cf15e07ceb875007c24ba784bfe0574962d98a27
Mariana Navarro <maronare10@gmail.com>
2023-11-21 00:48:56 -0500

    usando useState

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react"
+
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsernameInput = (event) => {
+    const value = event.target.value
+
+    setUsername(value)
+  }
+
+  const handlePasswordInput = (event) => {
+    const value = event.target.value
+
+    setPassword(value)
+  }
+  
   return (
     <>
       <h2 className="text-center text-4xl font-bold text-emerald-500">Superheroes</h2>
 
       <p className='text-gray-500 font-light text-center'>Ingresa un nombre de usuario y elige a tus super favoritos.</p>
+
+      {/*
+        {username}
+        {password}
+      */}
 
       <form className="mt-6 space-y-4" >
         <input
@@ -13,6 +35,7 @@ const Login = () => {
           required
           className="w-full appearance-none rounded border border-gray-300 px-4 py-4 text-gray-900 placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
           placeholder="Username"
+          onChange={handleUsernameInput}
         />
         <input
           autoFocus
@@ -21,13 +44,13 @@ const Login = () => {
           required
           className="w-full appearance-none rounded border border-gray-300 px-4 py-4 text-gray-900 placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
           placeholder="Password"
+          onChange={handlePasswordInput}
         />
 
         <button className="w-full rounded-md border border-transparent bg-indigo-600 py-4 px-4 font-medium text-white hover:bg-indigo-700 focus:outline-none">
           Ingresar
         </button>
       </form>
-
     </>
   )
 }
